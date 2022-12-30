712f8c9e24f498ada77cb882a72e5863536885de
chaosnabilera7 <chaosnabilera7@gmail.com>
2022-12-30 21:14:59 +0900

    refined examples\network\simple_socket_echo\windows_cpp\cmake_build.bat added .gitignore

--- network/simple_socket_echo/windows_cpp/cmake_build.bat
+++ network/simple_socket_echo/windows_cpp/cmake_build.bat
@@ -4,23 +4,8 @@
 rd /s /q build_debug
 rd /s /q build_release
 
-@REM if not exist output
-@REM     mkdir output
-@REM if not exist output\debug
-@REM     mkdir output\debug
-@REM if not exist output\release
-@REM     mkdir output\release
-
-@REM Note that setting CMAKE_GENERATOR_TOOLSET doesn't work very well. just use cmake -T for setting toolset
-
-@REM cmake -A Win32 -B debug/x86 -T v141_xp -DBUILDDEBUG=TRUE
-@REM cmake -A x64   -B debug/x64 -T v141_xp -DBUILDDEBUG=TRUE
-@REM cmake -A Win32 -B release/x86 -T v141_xp
-@REM cmake -A x64   -B release/x64 -T v141_xp
-@REM cmake --build debug/x86   --config Debug
-@REM cmake --build debug/x64   --config Debug
-@REM cmake --build release/x86 --config Release
-@REM cmake --build release/x64 --config Release
+@REM Note : CMAKE_RUNTIME_OUTPUT_DIRECTORY in CMakeLists.txt sets the runtime otuput directory
+@REM Note : setting CMAKE_GENERATOR_TOOLSET doesn't work very well. just use cmake -T for setting toolset
 
 cmake -A Win32 -B build_debug/x86 -DBUILDDEBUG=TRUE
 cmake -A x64   -B build_debug/x64 -DBUILDDEBUG=TRUE
@@ -30,3 +15,4 @@ cmake --build build_debug/x86   --config Debug
 cmake --build build_debug/x64   --config Debug
 cmake --build build_release/x86 --config Release
 cmake --build build_release/x64 --config Release
+
